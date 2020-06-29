73042365db32061686fc19a2eae327c3883894a2
langstonBS <langston.beckwithstanley@gmail.com>
2020-06-29 14:31:15 -0700

    added seed data and rout

---
+++ db/seed.js
@@ -0,0 +1,25 @@
+const chance = require('chance').Chance();
+const User = require('../lib/models/User');
+const Post = require('../lib/models/Post');
+const Comment = require('../lib/models/Comment');
+
+
+module.exports = async({ users = 20, posts = 100, comment = 500 } = {}) => {
+  const createdUsers = await User.create([...Array(users)].map(() => ({
+    username: chance.name(),
+    profilePhotoUrl: chance.url(),
+    password: chance.word()
+  })));
+  const createdPosts = await Post.create([...Array(posts)].map(() => ({
+    user: chance.pickone(createdUsers).id,
+    photoUrl: chance.url(),
+    caption: chance.sentence(),
+    tags: [chance.word(), chance.word()]
+  })));
+  await Comment.create([...Array(comment)].map(() => ({
+    commentBy: chance.pickone(createdUsers).id,
+    post: chance.pickone(createdPosts).id,
+    comment: chance.sentence()
+
+  })));
+};
